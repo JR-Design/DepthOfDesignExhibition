b7e4a9db827c1b411fb52eceed0d7d70cc56a1d7
matthewbacon4 <matthewbacon4@gmail.com>
2021-04-17 20:13:58 -0400

    Hunter

--- Scripts/countdown.js
+++ Scripts/countdown.js
@@ -4,13 +4,13 @@
           hour = minute * 60,
           day = hour * 24;
   
-    let birthday = "April 16, 2021 14:06:30",
+    let birthday = "April 22, 2021 18:00:00",
         countDown = new Date(birthday).getTime(),
         x = setInterval(function() {    
   
           let now = new Date().getTime(),
               distance = countDown - now;
-  
+              // document.getElementById("days").innerText = Math.floor(distance / (day)),
             document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
             document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
             document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
